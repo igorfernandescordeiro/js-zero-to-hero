07bf9e92c29f51d358cf290926fca698b9863036
Igor Cordeiro <ifcigor@gmail.com>
2024-05-23 22:37:41 -0300

    Learning about types of data and variables

--- notes/chapter_2.js
+++ notes/chapter_2.js
@@ -1,1 +1,10 @@
 console.log('Hello World');
+
+// Learned about data types and types of variables
+// usually variables are created with let or const, let is if the variable need to be changed
+// and the const is if the variable do not needed to be changed, but if it is an object type it could be changed, but what will be changed is the value not the key
+// Learned the types... boolean, string, number. function...
+// talked about parameters in a function
+//
+
+// Talking about operators
