9619d4bb5bc0868413f625cdf9a327ef0f2e2f06
Igor Cordeiro <ifcigor@gmail.com>
2024-05-27 20:20:14 -0300

    Learning about data manipulation, strings, methods of arrays and strings.

---
+++ notes/chapter_3.js
@@ -0,0 +1,25 @@
+// data manipulation 
+
+let example_sentence = 'is a string';
+let string_length = example_sentence.length;
+
+console.log('Value at the end of the string = ', example_sentence[string_length -1]);
+
+
+let contains_the_letter_a = example_sentence.indexOf('a');
+
+console.log(contains_the_letter_a);
+
+const split_sentence = example_sentence.split('');
+
+console.log(split_sentence);
+
+const inclues_or_not = example_sentence.includes('s');
+
+console.log(inclues_or_not);
+
+const slice_string = example_sentence.slice(0, 3);
+
+console.log(slice_string);
+
+//talk about regex
