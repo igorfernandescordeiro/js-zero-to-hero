6ef608b3e400a03b76bc0a20e704f78ec98d060e
Igor Cordeiro <ifcigor@gmail.com>
2024-05-28 17:18:32 -0300

    Learning more about arrays, objects and manipulating data... nested arrays..

--- notes/chapter_3.js
+++ notes/chapter_3.js
@@ -23,3 +23,34 @@ const slice_string = example_sentence.slice(0, 3);
 console.log(slice_string);
 
 //talk about regex
+// Regex stands for regular expressions 
+// Regex is a little bit complicated but if I need to use it someday, just look how to write a regex check for a passaword.
+
+
+// talking about arrays and objects
+
+let simple_array = ['igor', 'Fernandes', 'Cordeiro' ];
+
+//crud - create read update delete
+
+//pop push shift unshift
+
+console.log(simple_array);
+// add a new item in the end of the array
+simple_array.push('new world');
+
+console.log(simple_array);
+
+// remove the last item from an array
+simple_array.pop();
+
+console.log(simple_array);
+
+// join a array and turned into a string because as an array of string
+let check = simple_array.join(' ');
+
+console.log(check);
+
+let check2 = simple_array.sort();
+
+console.log(check2);
