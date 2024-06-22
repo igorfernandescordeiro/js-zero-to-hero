cad957e277bc61d03541651feb349b6ef9641158
Igor Cordeiro <ifcigor@gmail.com>
2024-06-22 20:20:54 -0300

    chapter 5, learning about method inside a class and how we can call it after

--- notes/chapter_5.js
+++ notes/chapter_5.js
@@ -10,10 +10,16 @@ class Person {
         this.name = name; // name is a property of the class
         this.age = age;
     }
+
+    greet() {
+        console.log(`hello my name is ${this.name}`);
+    };
+
+
 };
 
 const you = new Person('igor', 34);
 const them = new Person('jhones', 37);
 
 console.log(you);
-console.log(them);
+console.log(them.greet());
