5d36291e09abb04ce7a3d6ab9916bb6be99b9d84
Igor Cordeiro <ifcigor@gmail.com>
2024-06-22 20:15:33 -0300

    chapter 5, learning about class and constructor

--- notes/chapter_5.js
+++ notes/chapter_5.js
@@ -1,1 +1,19 @@
 // OOP
+
+const me = {
+
+};
+
+class Person {
+    // class body
+    constructor(name, age) {
+        this.name = name; // name is a property of the class
+        this.age = age;
+    }
+};
+
+const you = new Person('igor', 34);
+const them = new Person('jhones', 37);
+
+console.log(you);
+console.log(them);
