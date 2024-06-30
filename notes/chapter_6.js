dfb28a5b1dc8eb143b629995652b634541aa599f
Igor Cordeiro <ifcigor@gmail.com>
2024-06-30 12:22:13 -0300

    Chapter 6, learning about get a response from an endpoint

--- notes/chapter_6.js
+++ notes/chapter_6.js
@@ -2,12 +2,28 @@ const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 async function run(){
     console.log('Start Code');
-    
-    await sleep(2000);
+    await sleep(20);
     console.log('Half Way');
-    await sleep(2000);
+    await sleep(20);
     console.log('Finished Code');
     
 }
 
 run();
+
+
+async function fetchData(){
+
+    try {
+        const response = await fetch('https://dummyjson.com/products/1');
+        const data = await response.json();
+        console.log(data);
+        // console.log(response)
+    } catch(err) {
+        console.log(err.message);
+    }
+
+};
+
+
+fetchData();
