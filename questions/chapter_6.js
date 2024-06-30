29870d6bf496644651e29c6c22c094a03d380c21
Igor Cordeiro <ifcigor@gmail.com>
2024-06-30 12:26:00 -0300

    Preparing the file to the exercise for chapter 6

---
+++ questions/chapter_6.js
@@ -0,0 +1,15 @@
+
+// 1. Create a function that uses the following asynchronous sleep call to print the message 'hello world' after 2 seconds
+
+
+
+// 2. Write an async function that attempts to fetch data from 'https://api.example.com/nonexistent', which will likely lead to a 404 error. Use try/catch to handle the error and log "Error fetching data" if the request fails.
+
+
+
+// 3. Write an async function named fetchUserData that makes a GET request to 'https://the-one-api.dev/v2/' using async/await. Log the response to the console, handling any errors that might occur.
+
+
+
+// 4. Demonstrate how to make a POST request to 'https://api.example.com/users' to create a new user with JSON data { name: "John", age: 30 } using async/await. Ensure you set the appropriate headers and catch any errors.
+
