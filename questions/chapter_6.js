f37e73c928ce76e0e08b95b389cea81404b3ff57
Igor Cordeiro <ifcigor@gmail.com>
2024-07-02 17:21:06 -0300

    Exercise 2, chapter 6, fetching a data from an api

--- questions/chapter_6.js
+++ questions/chapter_6.js
@@ -16,6 +16,20 @@ waitForHelloWorld();
 
 // 2. Write an async function that attempts to fetch data from 'https://api.example.com/nonexistent', which will likely lead to a 404 error. Use try/catch to handle the error and log "Error fetching data" if the request fails.
 
+async function attemptsFetch(){
+  
+  try {
+    const response =  await fetch('https://api.example.com/nonexistent');
+    // console.log(response)
+    // const data = await response.json()
+    // console.log(data);
+  }
+    catch(error) {
+      console.log(error.message + ' aaaa');
+    }
+};
+attemptsFetch();
+
 
 
 // 3. Write an async function named fetchUserData that makes a GET request to 'https://the-one-api.dev/v2/' using async/await. Log the response to the console, handling any errors that might occur.
