18a9fbb3d131ff9882c1dd36def9840760ac2c02
Igor Cordeiro <ifcigor@gmail.com>
2024-07-06 10:48:19 -0300

    Exercise 4, chapter 6, idea how to create a post method

--- questions/chapter_6.js
+++ questions/chapter_6.js
@@ -2,12 +2,12 @@
 // 1. Create a function that uses the following asynchronous sleep call to print the message 'hello world' after 2 seconds
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  };
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
 
-async function waitForHelloWorld(){
-    await sleep(2000);
-    console.log('Hello World');
+async function waitForHelloWorld() {
+  await sleep(2000);
+  console.log('Hello World');
 };
 
 waitForHelloWorld();
@@ -16,28 +16,57 @@ waitForHelloWorld();
 
 // 2. Write an async function that attempts to fetch data from 'https://api.example.com/nonexistent', which will likely lead to a 404 error. Use try/catch to handle the error and log "Error fetching data" if the request fails.
 
-async function attemptsFetch(){
-  
+async function attemptsFetch() {
+
   try {
-    const response =  await fetch('https://api.example.com/nonexistent');
+    const response = await fetch('https://api.example.com/nonexistent');
     // console.log(response)
     // const data = await response.json()
     // console.log(data);
   }
-    catch(error) {
-      console.log("Error fetching data");
-    }
+  catch (error) {
+    console.log("Error fetching data");
+  }
 };
 attemptsFetch();
 
 
 
 // 3. Write an async function named fetchUserData that makes a GET request to 'https://the-one-api.dev/v2/' using async/await. Log the response to the console, handling any errors that might occur.
-async function fetchUserData {
-  const response = 
-}
+async function fetchUserData() {
+  try {
+    const response = await fetch('https://the-one-api.dev/v2/');
+    const data = await response.json();
+    console.log(data);
+    console.log(response.body);
+  } catch (error) {
+    console.log(`Erro: ${error}`);
+  }
+};
+fetchUserData();
 
 
 
 // 4. Demonstrate how to make a POST request to 'https://api.example.com/users' to create a new user with JSON data { name: "John", age: 30 } using async/await. Ensure you set the appropriate headers and catch any errors.
 
+async function postNewUser() {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ name: 'IGOR' })
+    }
+
+    const response = await fetch('https://api.example.com/users', options);
+
+    console.log(response.body);
+
+
+  } catch (error) {
+    console.log('POST ERROR: ' + error);
+  }
+}
+
+postNewUser();
