100554f718c41661f32f22f55041d6ccd5d28143
Igor Cordeiro <ifcigor@gmail.com>
2024-07-09 21:27:20 -0300

    Learning about arrow function

---
+++ notes/chapter_7.js
@@ -0,0 +1,21 @@
+// arrow functions
+
+
+
+// normal function
+function myFunction(input) {
+    // code that is repeatable
+};
+
+// arrow function
+(input) => { 
+    console.log('Executed function');
+};
+
+// naming an arrow function
+const myFunction2 = (input) => { 
+    console.log('Executed function');
+};
+
+
+// Ternary operator ? 
