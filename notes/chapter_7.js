4f24885b21ccdfb49fbe8f9f5c27ca6839166845
Igor Cordeiro <ifcigor@gmail.com>
2024-07-10 20:29:18 -0300

    Learned about optional chaining witch is very usefull for a safer code

--- notes/chapter_7.js
+++ notes/chapter_7.js
@@ -38,3 +38,16 @@ friends.includes('igor') ? isIgorFriend2 = true : isIgorFriend2 = false;
 console.log(isIgorFriend2);
 
 // optional chaining
+
+const igor = {
+    hobbies: {
+        soccer: {
+            pairs_of_boots: 1
+        }
+    }
+};
+// using the optional chaining witch is '?' 
+// if in this case return an undefined it will no longer brake the code, just return undefined
+console.log(igor?.hobbies?.skiing?.pairs_of_boots);
+
+
