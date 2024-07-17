5ebde1073886322ed291bdcae568684c95ad654c
Igor Cordeiro <ifcigor@gmail.com>
2024-07-16 21:11:48 -0300

    Preparing the file for the chapter 7 exercise and answering the first one

---
+++ questions/chapter_7.js
@@ -0,0 +1,53 @@
+
+// 1. How do you create a string that includes both static text and the value of the variable name, using template literals?
+
+const myName = 'igor';
+
+console.log(`My name is ${myName}`);
+
+
+
+// 2. Convert the following function into an arrow function syntax:
+
+  
+  // 3. Given an object person with properties name and age, use object destructuring to create two variables name and age, extracting the values from the object.
+  
+
+  // 4. Given an array colors with the values ['red', 'green', 'blue'], use array destructuring to create three variables named first, second, and third, assigning the first, second, and third elements of the array to them, respectively.
+  
+
+  
+  // 5. How do you create a new array combined that contains all elements of arrays arr1 and arr2 using the spread operator?
+ 
+  
+  // 6. How do you create a new object combinedObject that merges the properties of two objects obj1 and obj2, with properties from obj2 overwriting those in obj1 if they exist in both objects?
+  
+
+  
+  // 7. How do you safely access the street property of address inside an object user that may not exist, using optional chaining?
+ 
+  
+  // 8. Define a function greet(name, greeting = "Hello") that takes a name and an optional greeting. If the greeting is not provided, it should default to "Hello".
+
+  
+  // 9. Explain the difference between let and const keywords, and give an example of when you would use each.
+  
+ 
+  
+  // 10. Write a for...of loop that iterates over an array numbers and logs each number to the console.
+  
+
+  
+  // 11. Given an array of numbers, use the map method to create a new array with each number squared.
+ 
+  
+  // 12. How would you use the ternary operator to assign "adult" to a variable ageStatus if age is 18 or over, and "minor" if under 18?
+  
+
+  
+  // 13. How do you use the logical AND operator to execute a function logMessage() only if the variable isLoggedIn is true?
+  
+
+  
+  // 14. How can you use the logical OR operator to assign a default value of "guest" to a variable username if the current user.name is undefined or null?
+  
