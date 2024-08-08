1521566464095f7fcf05a00fdf4414f0a025f6d0
Igor Cordeiro <ifcigor@gmail.com>
2024-08-07 21:01:17 -0300

    Learning about the importance of having good names for varibles and functions, learning about best practices

--- notes/chapter_8.js
+++ notes/chapter_8.js
@@ -2,3 +2,9 @@
 
     // Minimise DOM manipulations
         // Efficient event listeners
+
+        let a = 4;
+        let b = 'igor';
+        // let
+
+// Have in mind that we always should have good names for variables and functions
