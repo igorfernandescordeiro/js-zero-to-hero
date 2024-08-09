9eada272707cd5380035b00382e7a3d8a4a2d89a
Igor Cordeiro <ifcigor@gmail.com>
2024-08-09 17:49:52 -0300

    Starting exercises for chapter 8 but this question is for thhe entire course

---
+++ questions/chapter_8.js
@@ -0,0 +1,27 @@
+// 1. Unique Characters in a String
+// Question: Write a function to determine if a string has all unique characters. Return true if all characters are unique, and false otherwise. Assume the string only contains lowercase letters.
+
+
+
+// 2. Merge Sorted Arrays
+// Question: Given two sorted arrays nums1 and nums2, write a function to merge them into a single, sorted array.
+
+
+
+
+// 3. Find All Duplicates in an Array
+// Question: Given an array of integers where 1 ≤ a[i] ≤ n (n = size of the array), some elements appear twice and others appear once. Write a function that returns an array of all the elements that appear twice in the given array. You must achieve this with O(n) time complexity.
+
+
+
+// 4. Rotate Array
+// Question: Rotate an array to the right by k steps, where k is non-negative. 
+
+
+
+// 5. Valid Parentheses
+// Question: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid. An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+
