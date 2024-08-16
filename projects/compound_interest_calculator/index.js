0fe27983990d16e9acb13f3e5ceb862ea74125e6
Igor Cordeiro <ifcigor@gmail.com>
2024-08-15 21:56:02 -0300

    Initialzing the first project

--- projects/compound_interest_calculator/index.js
+++ projects/compound_interest_calculator/index.js
@@ -1,10 +1,17 @@
 const prompt = require('prompt-sync')()
 // Compound interest calculator that prompts a user for some inputs, and finally calculates the compounded interest value
 
+let init_amount = 20000
+let monthly_contribution = 400
+let number_of_years = 30
+let interest_rate = 10
+
 
 
 // step 1 - define a function that we can use to calculate the final value of the compounded interest
-
+function compoundInterest(init_amount, monthly_contribution, number_of_years, interest_rate){
+    
+}
 
 
 // step 2 - define a function that would calculate the difference (ie the effect that compounding has had)
