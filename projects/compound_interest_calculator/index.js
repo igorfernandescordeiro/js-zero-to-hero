e3dd23323476e6e02b734f8996771835cec88a83
Igor Cordeiro <ifcigor@gmail.com>
2024-08-15 21:48:27 -0300

    Preparing the file for the first project Compound Interest Calculator

---
+++ projects/compound_interest_calculator/index.js
@@ -0,0 +1,18 @@
+const prompt = require('prompt-sync')()
+// Compound interest calculator that prompts a user for some inputs, and finally calculates the compounded interest value
+
+
+
+// step 1 - define a function that we can use to calculate the final value of the compounded interest
+
+
+
+// step 2 - define a function that would calculate the difference (ie the effect that compounding has had)
+
+
+// step 3 - to create a run function that then prompts the user for all necessary inputs required to calculate the final amounts
+
+
+
+// step 4 - inside of said function, make a nice pretty print statement using a template literal string that gives the financial breakdown
+
