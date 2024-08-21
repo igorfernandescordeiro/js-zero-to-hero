73480b7410c8ac483ec286e57e85c8b6d64f3b23
Igor Cordeiro <ifcigor@gmail.com>
2024-08-21 20:10:40 -0300

    First project is done!

--- projects/compound_interest_calculator/index.js
+++ projects/compound_interest_calculator/index.js
@@ -1,11 +1,5 @@
-// const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')()
 // Compound interest calculator that prompts a user for some inputs, and finally calculates the compounded interest value
-
-let init_amount = 20000
-let monthly_contribution = 400
-let number_of_years = 30
-let interest_rate = 10
-
 
 
 // step 1 - define a function that we can use to calculate the final value of the compounded interest
@@ -27,20 +21,30 @@ function calculateRegular(init_amount, monthly_contribution, number_of_years) {
     return (init_amount + monthly_contribution * 12 * number_of_years).toFixed(2);
 };
 
-function printOutPut(init_amount, monthly_contribution, number_of_years, interest_rate){
-    let final_value = compoundInterest(init_amount, monthly_contribution, number_of_years, interest_rate);
-    let value_without_compounding = calculateRegular(init_amount, monthly_contribution, number_of_years);
-    let sumary = `INIT_AMOUT: ${init_amount} \nMONTHLY_CONTRIBUTION: ${monthly_contribution}\nNUMBER_OF_YEARS: ${number_of_years}\nINTEREST_RATE: ${interest_rate}\n\nFINAL_COMPOUNDED_VALUE: $${final_value}\nREGULAR_AMOUNT: $${value_without_compounding}\nDIFFERENCE: $${final_value - value_without_compounding}`;
-
-    console.log(sumary);
-}
 // printOutPut(init_amount, monthly_contribution, number_of_years, interest_rate);
 
 
 
 // step 3 - to create a run function that then prompts the user for all necessary inputs required to calculate the final amounts
 
+function run(){
+    let init_amount = parseInt(prompt('What is your initial investiment ($)?'));
+    let monthly_amount = parseInt(prompt('What is your monthly contribution ($)?'));
+    let number_of_years = parseInt(prompt('For how many years would you like to compound your investment?'));
+    let interest_rate = parseInt(prompt('What is your expected interest rate (%) over the years?'));
+
+    printOutPut(init_amount, monthly_amount, number_of_years, interest_rate);
+}
 
 
 // step 4 - inside of said function, make a nice pretty print statement using a template literal string that gives the financial breakdown
 
+function printOutPut(init_amount, monthly_contribution, number_of_years, interest_rate){
+    let final_value = compoundInterest(init_amount, monthly_contribution, number_of_years, interest_rate);
+    let value_without_compounding = calculateRegular(init_amount, monthly_contribution, number_of_years);
+    let sumary = `INIT_AMOUT: $${init_amount} \nMONTHLY_CONTRIBUTION: $${monthly_contribution}\nNUMBER_OF_YEARS: ${number_of_years}\nINTEREST_RATE: %${interest_rate}\n\nFINAL_COMPOUNDED_VALUE: $${final_value}\nREGULAR_AMOUNT: $${value_without_compounding}\nDIFFERENCE: $${final_value - value_without_compounding}`;
+
+    console.log(sumary);
+}
+
+run();
