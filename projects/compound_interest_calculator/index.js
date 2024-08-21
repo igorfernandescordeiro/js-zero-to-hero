cb72163db081444a74aeeb8ad7ebe5a5db4598c4
Igor Cordeiro <ifcigor@gmail.com>
2024-08-20 21:58:18 -0300

    project compound, step 2 completed

--- projects/compound_interest_calculator/index.js
+++ projects/compound_interest_calculator/index.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')()
+// const prompt = require('prompt-sync')()
 // Compound interest calculator that prompts a user for some inputs, and finally calculates the compounded interest value
 
 let init_amount = 20000
@@ -18,17 +18,24 @@ function compoundInterest(init_amount, monthly_contribution, number_of_years, in
         total += annual_contribution;
         total *= ((100 + interest_rate) / 100);
     }
-    return total;
+    return total.toFixed(2);
 }
-
-function calculateRegular(init_amount, monthly_contribution, number_of_years) {
-    return init_amount + monthly_contribution * 12 * number_of_years;
-}
-
-console.log(compoundInterest(init_amount,monthly_contribution,number_of_years,interest_rate))
 
 
 // step 2 - define a function that would calculate the difference (ie the effect that compounding has had)
+function calculateRegular(init_amount, monthly_contribution, number_of_years) {
+    return (init_amount + monthly_contribution * 12 * number_of_years).toFixed(2);
+};
+
+function printOutPut(init_amount, monthly_contribution, number_of_years, interest_rate){
+    let final_value = compoundInterest(init_amount, monthly_contribution, number_of_years, interest_rate);
+    let value_without_compounding = calculateRegular(init_amount, monthly_contribution, number_of_years);
+    let sumary = `INIT_AMOUT: ${init_amount} \nMONTHLY_CONTRIBUTION: ${monthly_contribution}\nNUMBER_OF_YEARS: ${number_of_years}\nINTEREST_RATE: ${interest_rate}\n\nFINAL_COMPOUNDED_VALUE: $${final_value}\nREGULAR_AMOUNT: $${value_without_compounding}\nDIFFERENCE: $${final_value - value_without_compounding}`;
+
+    console.log(sumary);
+}
+// printOutPut(init_amount, monthly_contribution, number_of_years, interest_rate);
+
 
 
 // step 3 - to create a run function that then prompts the user for all necessary inputs required to calculate the final amounts
