dc0b17a47606b4cf9b110781e88d7fd674075ba9
Igor Cordeiro <ifcigor@gmail.com>
2024-08-22 20:40:59 -0300

    Configuration is done for the second project AI Chatbot

--- projects/AI_chatbot/index.js
+++ projects/AI_chatbot/index.js
@@ -1,1 +1,14 @@
 //Initialize the chatgt api, and then we are going to prompt a user for a message, and continue the conversation until the user ens the file
+
+import {OpenAIApi, Configuration} from 'openai';
+import {createRequire} from 'module';
+const require = createRequire(import.meta.url);
+require('dotenv').confg();
+
+// step 1 - initialize chatgpt api
+
+// step 2 - create contex for the api (give it some personality)
+
+// step 3 - define the function to retrive the api message based on user input
+
+// step 4 - create a run function that requests a user input
