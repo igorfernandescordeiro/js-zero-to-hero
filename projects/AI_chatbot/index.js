a368a0ae5b05dd235c2d11c7571880377bf85141
Igor Cordeiro <ifcigor@gmail.com>
2024-08-23 18:46:38 -0300

    AI Chatbot project, step 1

--- projects/AI_chatbot/index.js
+++ projects/AI_chatbot/index.js
@@ -7,6 +7,14 @@ require('dotenv').confg();
 
 // step 1 - initialize chatgpt api
 
+const OPEN_SECRET_KEY = process.env.OPEN_SECRET_KEY;
+
+const configuration = new Configuration({
+    apiKey: OPEN_SECRET_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
 // step 2 - create contex for the api (give it some personality)
 
 // step 3 - define the function to retrive the api message based on user input
