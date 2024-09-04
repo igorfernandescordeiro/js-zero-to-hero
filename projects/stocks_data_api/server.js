713b3963ba95f14dfe5be75bbba3c07fd5d0d3e5
Igor Cordeiro <ifcigor@gmail.com>
2024-09-04 20:48:09 -0300

    Stocks data api project, creating the frist function

--- projects/stocks_data_api/server.js
+++ projects/stocks_data_api/server.js
@@ -1,5 +1,27 @@
 // step 1 - define the web scraper
 
+const cheerio = require('cheerio');
+
+let stockTicker = 'mrna';
+
+let type = 'history';
+
+async function scrapeData() {
+    try {
+        // step a - fetch the page html
+
+        const url = `https://finance.yahoo.com/quote/${stockTicker}/${type}?p=${stockTicker}`;
+
+        const response = await fetch(url);
+
+        const html = await response.text();
+
+        const $ = cheerio.load(html);
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
 
 // step 2 - initialize server that server up an html file that th user can play with
 
