8a632cbe8fd67d5bbace2af29bf2cdc4e62d4971
Igor Cordeiro <ifcigor@gmail.com>
2024-09-03 22:47:03 -0300

    Adding a few more things to the project stocks data api

---
+++ projects/stocks_data_api/server.js
@@ -0,0 +1,7 @@
+// step 1 - define the web scraper
+
+
+// step 2 - initialize server that server up an html file that th user can play with
+
+
+// step 3 - define api endpoints to access stock data and call webscraper
