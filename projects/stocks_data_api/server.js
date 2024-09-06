6b3c2265e61e55a66da4135e468a0a8470171f32
Igor Cordeiro <ifcigor@gmail.com>
2024-09-05 22:30:55 -0300

    Trying to make this projec work, I´m no able to fetch the website

--- projects/stocks_data_api/server.js
+++ projects/stocks_data_api/server.js
@@ -2,7 +2,7 @@
 
 const cheerio = require('cheerio');
 
-let stockTicker = 'mrna';
+let stockTicker = 'SMAR';
 
 let type = 'history';
 
@@ -11,17 +11,62 @@ async function scrapeData() {
         // step a - fetch the page html
 
         const url = `https://finance.yahoo.com/quote/${stockTicker}/${type}?p=${stockTicker}`;
+        console.log(`URL: ${url}`)
 
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
+            }
+        });
+
 
         const html = await response.text();
 
         const $ = cheerio.load(html);
 
+        const price_history = getPrices($);
+
+        console.log(price_history);
+
     } catch (error) {
         console.log(error.message)
     }
 }
+
+function getPrices(cher) {
+    const prices = cher('td:nth-child(6)').get().map((current_value) => {
+        return cher(current_value).text()
+    })
+    return prices
+}
+
+
+// async function scrapeData(ticker) {
+//     try {
+//         // step a - fetch the page html
+//         const url = `https://finance.yahoo.com/quote/${ticker}/${type}?p=${ticker}`
+//         console.log(`URL: ${url}`)
+//         const res = await fetch(url)
+//         const html = await res.text()
+//         console.log(`HTML: ${html}`);
+
+//         const $ = cheerio.load(html)
+//         const price_history = getPrices($)
+//         console.log(price_history)
+//         return price_history
+//     } catch (err) {
+//         console.log(err.message)
+//     }
+// }
+
+// function getPrices(cher) {
+//     const prices = cher('td').get().map((current_value) => {
+//         return cher(current_value).text()
+//     })
+//     return prices
+// }
+
+scrapeData();
 
 // step 2 - initialize server that server up an html file that th user can play with
 
