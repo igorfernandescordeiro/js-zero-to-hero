755771b2651bb0e31eb77f285621be98aba0350e
Igor Cordeiro <ifcigor@gmail.com>
2024-09-14 17:46:41 -0300

    Still not working but i'm going forward

--- projects/stocks_data_api/server.js
+++ projects/stocks_data_api/server.js
@@ -1,28 +1,28 @@
 // step 1 - define the web scraper
 const cheerio = require('cheerio');
 
-let stockTicker = 'SMAR';
+let stockTicker = 'pypl';
 
 let type = 'history';
 
 
 async function scrapeData() {
     try {
-        // step a - fetch the page html
-
         const url = `https://finance.yahoo.com/quote/${stockTicker}/${type}?p=${stockTicker}`
-        //`https://finance.yahoo.com/quote/${stockTicker}/${type}?p=${stockTicker}`;
         console.log(`URL: ${url}`)
 
         const response = await fetch(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+                'Origin': 'https://your-site.com',
+                'x-requested-with': 'XMLHttpRequest', 
             },
         });
 
 
 
         const html = await response.text();
+     
 
         const $ = cheerio.load(html);
 
@@ -43,34 +43,26 @@ function getPrices(cher) {
 }
 
 
-// async function scrapeData(ticker) {
-//     try {
-//         // step a - fetch the page html
-//         const url = `https://finance.yahoo.com/quote/${ticker}/${type}?p=${ticker}`
-//         console.log(`URL: ${url}`)
-//         const res = await fetch(url)
-//         const html = await res.text()
-//         console.log(`HTML: ${html}`);
-
-//         const $ = cheerio.load(html)
-//         const price_history = getPrices($)
-//         console.log(price_history)
-//         return price_history
-//     } catch (err) {
-//         console.log(err.message)
-//     }
-// }
-
-// function getPrices(cher) {
-//     const prices = cher('td').get().map((current_value) => {
-//         return cher(current_value).text()
-//     })
-//     return prices
-// }
-
-scrapeData();
 
 // step 2 - initialize server that server up an html file that th user can play with
 
+const express = require('express');
+const app = express();
+const port = 8383;
+
+//middleware
+app.use(express.json());
+app.use(require('cors')());
+app.use(express.static('public'));
+
+
+
+
+
+app.listen(port, () => { console.log(`server has started on port: ${port}`)});
+
+
 
 // step 3 - define api endpoints to access stock data and call webscraper
+
+app.post();
